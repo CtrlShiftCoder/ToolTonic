0c8f76b5c51691fb20fdfb2dc19fc84b0e52505d
Ignacio Camiletti <icamilettimellado@gmail.com>
2024-10-25 01:14:33 -0300

    Commit final

--- src/components/Menubar/Menubar.tsx
+++ src/components/Menubar/Menubar.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import {Menubar as PrimeMenubar} from 'primereact/menubar';
-import {Button} from "primereact/button";
-import {MenubarPropsType} from "../../types/menubar";
+import { Menubar as PrimeMenubar } from 'primereact/menubar';
+import { Button } from "primereact/button";
+import { MenubarPropsType } from "../../types/menubar";
 
-const Menubar: React.FC<MenubarPropsType> = ({model, onLogoClick, onGitHubClick}) => {
+const Menubar: React.FC<MenubarPropsType> = ({ model, onLogoClick }) => {
 
-    const start = <Button icon="pi pi-code" type="button" className="logo" onClick={onLogoClick}/>;
+    const start = <Button icon="pi pi-code" type="button" className="logo" onClick={onLogoClick} />;
 
-    const end = <Button label="GitHub" type="button" icon="pi pi-github"
-                        className="p-button-outlined p-button-help"
-                        data-testid="GitHub"
-                        onClick={onGitHubClick}/>;
+    const end = null;
 
     return <PrimeMenubar className="p-mb-6" model={model} start={start} end={end} />
 }
